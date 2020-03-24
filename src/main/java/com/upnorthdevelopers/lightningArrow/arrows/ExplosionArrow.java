f1bf76e9093c649d21e5f3c293528e41e2a4790e
Matthew Naruzny <matthew_naruzny@shaw.ca>
2020-03-23 23:27:17 -0600

    Modified ExplosionArrow to allow alternate explosion powers
    - Added new constructor to ExplosionArrow class to allow custom explosion sizes

--- src/main/java/com/upnorthdevelopers/lightningArrow/arrows/ExplosionArrow.java
+++ src/main/java/com/upnorthdevelopers/lightningArrow/arrows/ExplosionArrow.java
@@ -11,9 +11,15 @@ import java.util.UUID;
 public class ExplosionArrow implements SpecialArrow {
 
     private UUID uuid;
+    private Float power;
 
     public ExplosionArrow(UUID uuid){
+        this(uuid, 5f);
+    }
+
+    public ExplosionArrow(UUID uuid, Float power){
         this.uuid = uuid;
+        this.power = power;
     }
 
     @Override
@@ -24,10 +30,10 @@ public class ExplosionArrow implements SpecialArrow {
     @Override
     public void impact(ProjectileHitEvent event) {
         try{
-            event.getHitEntity().getWorld().createExplosion(event.getHitEntity().getLocation(), 5f);
+            event.getHitEntity().getWorld().createExplosion(event.getHitEntity().getLocation(), power);
         } catch (NullPointerException ex){
             try{
-                event.getHitBlock().getWorld().createExplosion(event.getHitBlock().getLocation(), 5f);
+                event.getHitBlock().getWorld().createExplosion(event.getHitBlock().getLocation(), power);
             } catch (NullPointerException ignored){
 
             }
